999ccc5d89c029f6d70f536581732a8ca124f93f
Kaustubh Pawar <167971412+pawar-kaustubh@users.noreply.github.com>
2025-04-18 16:30:42 +0530

    add startup model and usermodel

---
+++ api/models/startup.model.js
@@ -0,0 +1,93 @@
+import mongoose from "mongoose";
+const startupSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    startupname: {
+      type: String,
+      required: true,
+    },
+    startupdesc: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    industry: {
+      type: String,
+      required: true,
+    },
+    socials: {
+      type: String,
+      required: true,
+    },
+    team: {
+      type: String,
+      required: true,
+    },
+    totalsales: {
+      type: Number,
+      required: true,
+    },
+    revenue: {
+      type: Number,
+      required: true,
+    },
+    profit: {
+      type: Number,
+      required: true,
+    },
+    valuation: {
+      type: Number,
+      required: true,
+    },
+    equity: {
+      type: Number,
+      required: true,
+    },
+    pitchdeck: {
+      type: String,
+      required: true,
+    },
+    burnrate: {
+      type: Number,
+      required: true,
+    },
+    runway: {
+      type: Number,
+      required: true,
+    },
+    youtube: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Startup = mongoose.model("Startup", startupSchema);
+export default Startup;
