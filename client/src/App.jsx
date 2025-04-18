1c4f07fe3734a8fa4faf7e1c89a9155ff895e2d7
Amey <ameygawade11@gmail.com>
2025-04-18 19:13:51 +0530

    startup added on App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Layout from "./Layout";
 import SignIn from "./pages/SignIn";
 
 import Navbar from "./components/Navbar";
 import SignUp from "./pages/SignUp";
 import ExploreStartUp from "./pages/ExploreStartUp";
+import StartUpDetails from "./pages/StartupDetails";
 
 
 export default function App() {
@@ -19,6 +19,7 @@ export default function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/explorestartups" element={<ExploreStartUp />} />
+        <Route path="/startupdetails" element={<StartUpDetails />} />
       </Routes>
 
       </>
