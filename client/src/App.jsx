aaf6bd3b8f293ae17e79ed8cf98e4e59299e1264
unlimitedcode07 <kwwhiuwhefk@gmail.com>
2025-04-19 13:18:43 +0530

    sign page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import InvestorForm from "./pages/InvestorForm";
-import StartupDashboard from "./pages/StartupDashboard";
+// import StartupDashboard from "./pages/StartupDashboard";
 
 
 export default function App() {
@@ -26,6 +26,7 @@ export default function App() {
         <Route path="/startupdetails" element={<StartUpDetails />} />
         <Route path="/startupform" element={<StartUpForm />} />
         <Route path="/investorform" element={<InvestorForm />} />
+        <Route path="/login" element={<Login/>} />
         <Route element={<PrivateRoute />}>
            <Route path='/profile' element={<Profile />} />
          </Route>
